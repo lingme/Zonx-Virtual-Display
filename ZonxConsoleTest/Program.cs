a020f7262f7a1c5cc6d0fc1faa5a9cde1f73b2d6
lingme <kid--l@hotmail.com>
2020-04-19 23:56:02 +0800

    add load driver code

--- ZonxConsoleTest/Program.cs
+++ ZonxConsoleTest/Program.cs
@@ -1,20 +1,33 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Runtime.InteropServices;
+using System.Text;
+using ZonxWinAPI;
 
 namespace ZonxConsoleTest
 {
     class Program
     {
-        [DllImport("Setupapi.dll", EntryPoint = "InstallHinfSection", CallingConvention = CallingConvention.StdCall)]
-        public static extern void InstallHinfSection(
-            [In] IntPtr hwnd,
-            [In] IntPtr ModuleHandle,
-            [In, MarshalAs(UnmanagedType.LPWStr)] string CmdLineBuffer,
-            int nCmdShow);
-
         static void Main(string[] args)
         {
-            Console.WriteLine("=================Test");
+            Console.WriteLine("=================Test Install Inf=================\r\n");
+
+
+            var infPath = @"C:\ZonxVirtualDevice\ZonxVirtualDevice.inf";
+
+            int size = 0;
+            bool success = InstallHinf.SetupCopyOEMInf(infPath, "", OemSourceMediaType.SPOST_NONE, OemCopyStyle.SP_COPY_NEWER, null, 0, ref size, null);
+
+            if (!success)
+            {
+                var errorCode = Marshal.GetLastWin32Error();
+                var errorString = new Win32Exception(errorCode).Message;
+                Console.WriteLine(errorString);
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("Press any key exit program");
+            Console.ReadKey();
         }
     }
 }
--- ZonxWinAPI/InstallHinf.cs
+++ ZonxWinAPI/InstallHinf.cs
@@ -1,8 +1,43 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
 
 namespace ZonxWinAPI
 {
-    public class Class1
+    public enum OemSourceMediaType
     {
+        SPOST_NONE = 0,
+        SPOST_PATH = 1,
+        SPOST_URL = 2,
+        SPOST_MAX = 3
+    }
+
+    public enum OemCopyStyle
+    {
+        SP_COPY_NEWER = 0x0000004,
+        SP_COPY_NEWER_ONLY = 0x0010000,
+        SP_COPY_OEMINF_CATALOG_ONLY = 0x0040000,
+    }
+
+    public class InstallHinf
+    {
+        [DllImport("Setupapi.dll", EntryPoint = "InstallHinfSection", CallingConvention = CallingConvention.StdCall)]
+        public static extern void InstallHinfSection(
+            [In] IntPtr hwnd,
+            [In] IntPtr ModuleHandle,
+            [In, MarshalAs(UnmanagedType.LPWStr)] string CmdLineBuffer,
+            int nCmdShow);
+
+        [DllImport("setupapi.dll", SetLastError = true)]
+        public static extern bool SetupCopyOEMInf(
+            string SourceInfFileName,
+            string OEMSourceMediaLocation,
+            OemSourceMediaType OEMSourceMediaType,
+            OemCopyStyle CopyStyle,
+            string DestinationInfFileName,
+            int DestinationInfFileNameSize,
+            ref int RequiredSize,
+            string DestinationInfFileNameComponent
+        );
     }
 }
