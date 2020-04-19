8cf05cbb25aaab9351056aa61899864700336ca0
lingme <kid--l@hotmail.com>
2020-04-19 21:29:40 +0800

    add .net core wpf client project

--- ZonxConsoleTest/Program.cs
+++ ZonxConsoleTest/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace ZonxConsoleTest
 {
     class Program
     {
+        [DllImport("Setupapi.dll", EntryPoint = "InstallHinfSection", CallingConvention = CallingConvention.StdCall)]
+        public static extern void InstallHinfSection(
+            [In] IntPtr hwnd,
+            [In] IntPtr ModuleHandle,
+            [In, MarshalAs(UnmanagedType.LPWStr)] string CmdLineBuffer,
+            int nCmdShow);
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("=================Test");
         }
     }
 }
