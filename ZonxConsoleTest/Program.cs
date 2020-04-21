87c3f8fa58fb090045a30daee76bff6a9c0f50b8
lingme <kid--l@hotmail.com>
2020-04-21 23:49:17 +0800

    add install hinf uninstall oem inf

--- ZonxConsoleTest/Program.cs
+++ ZonxConsoleTest/Program.cs
@@ -18,7 +18,9 @@ namespace ZonxConsoleTest
             //InstallHinf.InstallHinfSection(IntPtr.Zero, IntPtr.Zero, infPath, 0);
 
 
-            InstallHinf.SetupCopyOEMInf(infPath, null, 0, 0, null, 0, 0, null);
+            //InstallHinf.SetupCopyOEMInf(infPath, null, 0, 0, null, 0, 0, null);
+
+            InstallHinf.SetupUninstallOEMInfA(infPath, 0, IntPtr.Zero);
 
             Console.WriteLine("Press any key exit program");
             Console.ReadKey();
--- ZonxWinAPI/InstallHinf.cs
+++ ZonxWinAPI/InstallHinf.cs
@@ -4,66 +4,8 @@ using System.Text;
 
 namespace ZonxWinAPI
 {
-    public enum OemSourceMediaType
-    {
-        SPOST_NONE = 0,
-        SPOST_PATH = 1,
-        SPOST_URL = 2,
-        SPOST_MAX = 3
-    }
-
-    public enum OemCopyStyle
-    {
-        SP_COPY_NEWER = 0x0000004,
-        SP_COPY_NEWER_ONLY = 0x0010000,
-        SP_COPY_OEMINF_CATALOG_ONLY = 0x0040000,
-    }
-
     public class InstallHinf
     {
-        private const int SC_MANAGER_CREATE_SERVICE = 2;
-        private const int SERVICE_START = 16;
-        private const int SERVICE_KERNEL_DRIVER = 1;
-        private const int SERVICE_DEMAND_START = 3;
-        private const int SERVICE_ERROR_IGNORE = 0;
-        private readonly static IntPtr NULL = IntPtr.Zero;
-
-        [DllImport("advapi32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-        public static extern IntPtr OpenSCManager(string machineName, string databaseName, uint dwAccess);
-
-        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-        public static extern IntPtr CreateService(IntPtr hSCManager, string lpServiceName, string lpDisplayName, int dwDesiredAccess, int dwServiceType, int dwStartType, int dwErrorControl, string lpBinaryPathName, int lpLoadOrderGroup, int lpdwTagId, int lpDependencies, int lpServiceStartName, int lpPassword);
-
-        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-        private static extern IntPtr OpenService(IntPtr hSCManager, string lpServiceName, uint dwDesiredAccess);
-
-        [DllImport("advapi32.dll", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool CloseServiceHandle(IntPtr hSCManager);
-
-        [DllImport("advapi32.dll", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool StartService(IntPtr hService, int dwNumServiceArgs, string[] lpServiceArgVectors);
-
-        [DllImport("Setupapi.dll", EntryPoint = "InstallHinfSection", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Unicode)]
-        public static extern void InstallHinfSection(
-            [In] IntPtr hwnd,
-            [In] IntPtr ModuleHandle,
-            [In, MarshalAs(UnmanagedType.LPWStr)] string CmdLineBuffer,
-            int nCmdShow);
-
-        [DllImport("setupapi.dll", SetLastError = true)]
-        public static extern bool SetupCopyOEMInf(
-            string SourceInfFileName,
-            string OEMSourceMediaLocation,
-            OemSourceMediaType OEMSourceMediaType,
-            OemCopyStyle CopyStyle,
-            string DestinationInfFileName,
-            int DestinationInfFileNameSize,
-            ref int RequiredSize,
-            string DestinationInfFileNameComponent
-        );
-
         [DllImport("setupapi.dll")]
         public static extern bool SetupCopyOEMInf(
             string SourceInfFileName,
@@ -73,8 +15,7 @@ namespace ZonxWinAPI
             string DestinationInfFileName,
             int DestinationInfFileNameSize,
             int RequiredSize,
-            string DestinationInfFileNameComponent
-    );
+            string DestinationInfFileNameComponent);
 
         [DllImport("newdev.dll")]
         public static extern bool UpdateDriverForPlugAndPlayDevices(
@@ -82,7 +23,12 @@ namespace ZonxWinAPI
             string HardwareId,
             string FullInfPath,
             uint InstallFlags,
-            bool bRebootRequired
-            );
+            bool bRebootRequired);
+
+        [DllImport("setupapi.dll")]
+        public static extern bool SetupUninstallOEMInfA(
+            string InfFileName,
+            int Flags,
+            IntPtr Reserved);
     }
 }
