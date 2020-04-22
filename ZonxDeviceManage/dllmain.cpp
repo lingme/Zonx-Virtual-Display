50efce122713057b9c1295b7721748fbba58f5ac
lingme <kid--l@hotmail.com>
2020-04-22 23:57:23 +0800

    add create zonx virtual device

--- ZonxDeviceManage/dllmain.cpp
+++ ZonxDeviceManage/dllmain.cpp
@@ -1,5 +1,10 @@
 #include "pch.h"
 #include <iostream>
+#include <vector>
+#include <windows.h>
+#include <swdevice.h>
+#include <conio.h>
+#include <wrl.h>
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
@@ -14,10 +19,15 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
 	return TRUE;
 }
 
-extern "C" __declspec(dllexport) void Test();
-
-void Test()
+VOID WINAPI
+CreationCallback(_In_ HSWDEVICE hSwDevice, _In_ HRESULT hrCreateResult, _In_opt_ PVOID pContext,_In_opt_ PCWSTR pszDeviceInstanceId)
 {
-	std::cout << "Hello world" << std::endl;
+	HANDLE hEvent = *(HANDLE*)pContext;
+	SetEvent(hEvent);
 }
+
+
+
+//extern "C" __declspec(dllexport) void Test();
+
 
